3bf1701eb67a7edb302f96c8a8230675eb147117
artemb89 <artem_b89@mail.ru>
2016-05-23 17:25:38 +0300

    some fixes

--- src/app/Services/ApiSettings.ts
+++ src/app/Services/ApiSettings.ts
@@ -1,7 +1,7 @@
 export class ApiSettings
 {	
 	static baseUrl: string = "http://ruscount.com:9034";
-	static _api: string = '/api';
+	static _api: string = '/api/v1';
 	static _childs: string = '/childs';
 	
 	static _organization: string = '/organizations';
