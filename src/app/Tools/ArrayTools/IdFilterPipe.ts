23a8c329c5e2acca0f54fe7e1419455b417df6d4
qwsazxc88 <ber._.serk@bk.ru>
2016-06-21 22:58:50 +0300

    Вьюшка ролей без логики Очень странно придуманы профили было бы логично Profile[Roles][Modules].Permissions преобразовал ArrayTools в модуль

--- src/app/Tools/ArrayTools/IdFilterPipe.ts
+++ src/app/Tools/ArrayTools/IdFilterPipe.ts
@@ -1,5 +1,5 @@
 import {Pipe, PipeTransform} from '@angular/core';
-import {Referency} from '../../Model'
+import {Referency} from '../../Model';
 
 @Pipe({
   name: 'idFilter',
@@ -9,20 +9,19 @@ import {Referency} from '../../Model'
 export class IdFilterPipe implements PipeTransform {
   transform(value: any[], args: any[]) {
     var newArray: any[] = [];
-	var id:number = args[0];
+    var id: number = args[0];
     var subarrayname: string = args[1];
-    
+
     if (!value) return value;
     if (!id) return value;
-    console.log(id);
-    console.log(subarrayname);
-    if(subarrayname)
-    {
-        newArray = value.filter(x=>x[subarrayname].Id==id);
-    }
-    else
-    {
-        newArray = value.filter(x=>x.Id==id);
+    console.log('IdFilterPipe id ' + id);
+    console.log('IdFilterPipe subarr' + subarrayname);
+    console.dir(value);
+    console.dir(args);
+    if (subarrayname) {
+      newArray = value.filter(x => x[subarrayname].Id == id);
+    } else {
+      newArray = value.filter(x => x.Id == id);
     }
     return newArray;
   }
