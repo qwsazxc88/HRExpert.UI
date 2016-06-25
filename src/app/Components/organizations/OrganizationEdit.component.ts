167c22024f490c8bae299d99188857e60605afff
qwsazxc88 <ber._.serk@bk.ru>
2016-06-26 01:23:06 +0300

    форматирование / уточнение типов

--- src/app/Components/organizations/OrganizationEdit.component.ts
+++ src/app/Components/organizations/OrganizationEdit.component.ts
@@ -1,80 +1,72 @@
 //Vendor libs
-import {Component,OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouteParams} from '@angular/router-deprecated';
 
 //Libs
 import {API} from '../../Services';
-import {Organization,Department} from '../../Model';
+import {Organization, Department} from '../../Model';
 import {MD_COMPONENTS} from '../MD_COMPONENTS';
 import {DepartmentsListComponent} from '../departments/DepartmentsList.component';
 @Component({
     selector: 'organization-edit',
     template: require('../../Views/organizations/Edit.html'),
     directives: [DepartmentsListComponent, MD_COMPONENTS],
-	providers: [API]
+    providers: [API]
 })
 
-export class OrganizationEditComponent implements OnInit
-{ 
-	constructor (private Api: API, private _routeParams: RouteParams) 
-	{
-		this.Model=new Organization(0,'');  
-        let id =+this._routeParams.get('organizationid');
-		if(id>0)
-		{
-            this.Model.Id=id;
-			this.Get(id);
-		}      
-	}
+export class OrganizationEditComponent implements OnInit {
+    constructor(private Api: API, private _routeParams: RouteParams) {
+        this.Model = new Organization(0, '');
+        let id = +this._routeParams.get('organizationid');
+        if (id > 0) {
+            this.Model.Id = id;
+            this.Get(id);
+        }
+    }
 
     errorMessage: string;
     Model: Organization;
     Departments: Department[];
     ngOnInit() {
-		
+
     }
-	
-	Save()
-	{
-		var data = this.Model;
-        if(data.Id>0)
-		{
-			this.Api.Organizations().Update(data)
-				.subscribe(
-					result => {this.Model = result; },
-					error => this.errorMessage = <any>error
-				);
-		}
-		else
-		{
-			this.Api.Organizations().Create(data)
-				.subscribe(
-					result => {this.Model = result; },
-					error => this.errorMessage = <any>error
-				);
-		}
-	}
-	Delete()
-	{
-		var data = this.Model;
-		if(data.Id>0)
-		{
-			this.Api.Organizations(data.Id).Delete()
-				.subscribe(                                  
-					error => this.errorMessage = <any>error
-				);
-		}
-	}
+
+    Save() {
+        var data = this.Model;
+        if (data.Id > 0) {
+            this.Api.Organizations().Update(data)
+                .subscribe(
+                result => { this.Model = result; },
+                error => this.errorMessage = <any>error
+                );
+        }
+        else {
+            this.Api.Organizations().Create(data)
+                .subscribe(
+                result => { this.Model = result; },
+                error => this.errorMessage = <any>error
+                );
+        }
+    }
+    Delete() {
+        var data = this.Model;
+        if (data.Id > 0) {
+            this.Api.Organizations(data.Id).Delete()
+                .subscribe(
+                error => this.errorMessage = <any>error
+                );
+        }
+    }
     Get(id) {
         this.Api.Organizations(id).Read()
-			.subscribe(
-				result => {this.Model = result; },
-				error => this.errorMessage = <any>error
-			);
-		this.Api.Organizations(id).Departments().List()
-			.subscribe(
-				result => {this.Departments = result; },
-				error => this.errorMessage = <any>error
-			);
+            .subscribe(
+            result => { this.Model = result; },
+            error => this.errorMessage = <any>error
+            );
+        this.Api.Organizations(id).Departments().List()
+            .subscribe(
+            result => { this.Departments = result; },
+            error => this.errorMessage = <any>error
+            );
     }
 }
