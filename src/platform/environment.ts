0e65006d5c2043f16586c909b639f64de85ed364
qwsazxc88 <ber._.serk@bk.ru>
2016-07-12 23:01:58 +0300

    Updated package.json Removed polyfills from dev build

--- src/platform/environment.ts
+++ src/platform/environment.ts
@@ -4,43 +4,44 @@ import { enableDebugTools, disableDebugTools } from '@angular/platform-browser';
 import { enableProdMode } from '@angular/core';
 // Environment Providers
 let PROVIDERS = [
-  // common env directives
+    // common env directives
 ];
 
 // Angular debug tools in the dev console
 // https://github.com/angular/angular/blob/86405345b781a9dc2438c0fbe3e9409245647019/TOOLS_JS.md
 let _decorateComponentRef = function identity(value) { return value; };
 
-if ('production' !== ENV) {
-  // Production
-  disableDebugTools();
-  enableProdMode();
+if ('production' === ENV) {
+    // Production
+    disableDebugTools();
+    enableProdMode();
 
-  PROVIDERS = [
-    ...PROVIDERS,
-    // custom providers in production
-  ];
+    PROVIDERS = [
+        ...PROVIDERS,
+        // custom providers in production
+    ];
 
 } else {
+    // Development
+    enableProdMode(); /*// NOTE FIXME BUG HACK */
 
-  _decorateComponentRef = (cmpRef) => {
-    let _ng = (<any>window).ng;
-    enableDebugTools(cmpRef);
-    (<any>window).ng.probe = _ng.probe;
-    (<any>window).ng.coreTokens = _ng.coreTokens;
-    return cmpRef;
-  };
+    _decorateComponentRef = (cmpRef) => {
+        let _ng = (<any>window).ng;
+        enableDebugTools(cmpRef);
+        (<any>window).ng.probe = _ng.probe;
+        (<any>window).ng.coreTokens = _ng.coreTokens;
+        return cmpRef;
+    };
 
-  // Development
-  PROVIDERS = [
-    ...PROVIDERS,
-    // custom providers in development
-  ];
+    PROVIDERS = [
+        ...PROVIDERS,
+        // custom providers in development
+    ];
 
 }
 
 export const decorateComponentRef = _decorateComponentRef;
 
 export const ENV_PROVIDERS = [
-  ...PROVIDERS
+    ...PROVIDERS
 ];
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,17 +1,54 @@
 // Polyfills
-// (these modules are what are in 'angular2/bundles/angular2-polyfills' so don't use that here)
 
-// import 'ie-shim'; // Internet Explorer
-// import 'es6-shim';
-// import 'es6-promise';
-// import 'es7-reflect-metadata';
+// import 'ie-shim'; // Internet Explorer 9 support
 
-// Prefer CoreJS over the polyfills above
-import 'core-js';
+// import 'core-js/es6';
+// Added parts of es6 which are necessary for your project or your browser support requirements.
+/*import 'core-js/es6/symbol'; // NOTE FIXME BUG HACK
+import 'core-js/es6/object';
+import 'core-js/es6/function';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/number';
+import 'core-js/es6/math';
+import 'core-js/es6/string';
+import 'core-js/es6/date';
+import 'core-js/es6/array';
+import 'core-js/es6/regexp';
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+import 'core-js/es6/weak-map';
+import 'core-js/es6/weak-set';
+import 'core-js/es6/typed';
+import 'core-js/es6/reflect';*/
+// see issue https://github.com/AngularClass/angular2-webpack-starter/issues/709
+// import 'core-js/es6/promise';
+
+import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
+
+// Typescript emit helpers polyfill
+import 'ts-helpers';
 
 if ('production' === ENV) {
     // Production
+    require('core-js/es6/symbol'); /*// NOTE FIXME BUG HACK */
+    require('core-js/es6/object');
+    require('core-js/es6/function');
+    require('core-js/es6/parse-int');
+    require('core-js/es6/parse-float');
+    require('core-js/es6/number');
+    require('core-js/es6/math');
+    require('core-js/es6/string');
+    require('core-js/es6/date');
+    require('core-js/es6/array');
+    require('core-js/es6/regexp');
+    require('core-js/es6/map');
+    require('core-js/es6/set');
+    require('core-js/es6/weak-map');
+    require('core-js/es6/weak-set');
+    require('core-js/es6/typed');
+    require('core-js/es6/reflect');
 
 } else {
     // Development
